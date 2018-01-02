45f1b89104c2d53f71247e0a934d4f2d27cf5dd5
James A. Kupsch <kupsch@cs.wisc.edu>
2018-01-02 10:16:34 -0600

    fix trim function.
    Was returning a pointer to memory that was supposed to have been
    malloc'd, but was not.  reported by MIR team.

--- c/ScarfXmlReader.c
+++ c/ScarfXmlReader.c
@@ -15,6 +15,7 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <libxml/xmlreader.h>
@@ -330,14 +331,13 @@ char *trim(char *str)
     while ( isspace(*str) ) {
 	str++;
     }
-    if ( *str == 0 ) {
-	return str;
+
+    end = str + strlen(str);
+    while ( end > str && isspace(end - 1) ) {
+	--end;
     }
-    end = str + strlen(str) - 1;
-    while ( end > str && isspace(*end) ) {
-	end--;
-    }
-    *(end+1) = '\0';
+    *end = '\0';
+
     char *newStr = malloc(strlen(str) + 1);
     strcpy(newStr, str);
     return newStr;
